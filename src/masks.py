c00e16d0ddb85fa96dd9eab458fe792f0a34a4b7
Vadim Kiselev <klaim367@gmail.com>
2025-06-12 18:25:44 +0300

    the beginning of the project, the masking function has been added

---
+++ src/masks.py
@@ -0,0 +1,10 @@
+def get_mask_card_number(card_number: str) -> str:
+    """Функция принимает на вход номер карты и возвращает ее маску."""
+
+    return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+
+
+def get_mask_account(account: str) -> str:
+    """Функция принимает на вход номер счета и возвращает его маску."""
+
+    return f"**{account[-4:]}"
---
+++ tests/test.py
@@ -0,0 +1,9 @@
+from src.masks import get_mask_account, get_mask_card_number
+
+card_number = "7000792289606361"
+
+print(get_mask_card_number(card_number))
+
+account = "73654108430135874305"
+
+print(get_mask_account(account))
