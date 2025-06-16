a269b51d57fda086ebe009de05f0f9c3c8184078
Vadim Kiselev <klaim367@gmail.com>
2025-06-16 13:16:27 +0300

    added the mask_account_card function

---
+++ src/widget.py
@@ -0,0 +1,22 @@
+from src.masks import get_mask_account, get_mask_card_number
+
+
+def mask_account_card(account_card: str) -> str:
+    """Функция, которая обрабатывает информацию о картах и счетах"""
+
+    list_data = account_card.split()
+    count = -1
+    for data_card in list_data:
+
+        count += 1
+
+    if len(list_data[count]) <= 16:
+        mask = get_mask_card_number(list_data[count])
+    else:
+        mask = get_mask_account(list_data[count])
+    if count == 2:
+        result = f"{list_data[0]} {list_data[1]} {mask}"
+    else:
+        result = f"{list_data[0]} {mask}"
+
+    return result
--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,5 @@
 from src.masks import get_mask_account, get_mask_card_number
+from src.widget import mask_account_card
 
 card_number = "7000792289606361"
 
@@ -7,3 +8,7 @@ print(get_mask_card_number(card_number))
 account = "73654108430135874305"
 
 print(get_mask_account(account))
+
+account = "Visa Classic 6831982476737658"
+
+print(mask_account_card(account))
