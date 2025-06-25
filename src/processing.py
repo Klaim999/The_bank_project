13dc059f5962bb56a4b3463175dc3104c1e211a4
Vadim Kiselev <klaim367@gmail.com>
2025-06-25 11:47:43 +0300

    add function sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,16 @@
 def filter_by_state(list_dict: list, state_key: str) -> list:
+    """
+    Функция возвращает новый список словарей, содержащий только те словари, у которых ключ
+    соответствует указанному значению.
+    """
     new_list_dict = []
     for dict_ in list_dict:
-        if dict_["state"] == 'EXECUTED':
+        if dict_["state"] == "EXECUTED":
             new_list_dict.append(dict_)
 
     return new_list_dict
+
+
+def sort_by_date(dict_list: list, reverse: bool) -> list:
+    """Функция, которая сортирует дату по заданному параметру"""
+    return sorted(dict_list, key=lambda x: x["date"], reverse=reverse)
